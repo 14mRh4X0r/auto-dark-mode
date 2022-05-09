088860f8af6f096e4123f4085f2c05b859cbac6d
l0drex <hoffmann_lorenz@protonmail.com>
2022-05-09 10:38:52 +0200

    Create new class to hold methods that need the connection

--- linux/src/main/java/com/github/weisj/darkmode/platform/linux/LinuxThemeMonitorService.kt
+++ linux/src/main/java/com/github/weisj/darkmode/platform/linux/LinuxThemeMonitorService.kt
@@ -29,17 +29,16 @@ import com.github.weisj.darkmode.platform.Notifications
 import com.github.weisj.darkmode.platform.NullThemeMonitorService
 import com.github.weisj.darkmode.platform.ThemeMonitorService
 import com.github.weisj.darkmode.platform.linux.gtk.GtkThemeMonitorService
-import com.github.weisj.darkmode.platform.linux.xdg.FreedesktopInterface
-import com.github.weisj.darkmode.platform.linux.xdg.ThemeMode
 import com.github.weisj.darkmode.platform.linux.xdg.XdgThemeMonitorService
 
 class LinuxThemeMonitorService : ThemeMonitorService by createCompatibleMonitorService()
 
 private fun createCompatibleMonitorService() : ThemeMonitorService {
     if (LibraryUtil.isGtk) return GtkThemeMonitorService()
-    if (FreedesktopInterface.theme != ThemeMode.ERROR) return XdgThemeMonitorService()
+    val xdgThemeMonitorService = XdgThemeMonitorService()
+    if (xdgThemeMonitorService.isSupported) return xdgThemeMonitorService
     Notifications.dispatchNotification(
-        message = "This plugin currently only supports Gtk based desktop environment on Linux."
+        message = "This plugin currently only supports desktop environments that either are Gtk based, or support the color-scheme preference on Linux."
     )
     return NullThemeMonitorService()
 }
